5de1d9419667171a42d8355a7641cb95bf61f053
Omar Hosny <68732608+Omar-Hosni@users.noreply.github.com>
2024-05-21 15:25:54 +0200

    Update quad_tree2.cpp

--- quad_tree2.cpp
+++ quad_tree2.cpp
@@ -5,8 +5,6 @@
 
 using namespace std;
 
-
-
 struct Node {
     int val;
     bool isLeaf;
@@ -25,10 +23,40 @@ public:
         const auto n = grid.size();
         Node* root = new Node();
 
+        auto start_time = chrono::high_resolution_clock::now();
 
         rec(root, grid, 0, 0, n);
 
-        
+        auto end_time = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
+
+        cout << "Time taken for a tree construction: " << duration.count() << " microseconds" << endl;
+
+        return root;
+    }
+
+    Node* constructParallel(vector<vector<int>>& grid) {
+        const auto n = grid.size();
+        Node* root = new Node();
+
+        auto start_time = chrono::high_resolution_clock::now();
+
+        // Construct the quad tree in parallel
+        thread t1([&]() { rec(root->topLeft = new Node(), grid, 0, 0, n / 2); });
+        thread t2([&]() { rec(root->topRight = new Node(), grid, 0, n / 2, n / 2); });
+        thread t3([&]() { rec(root->bottomLeft = new Node(), grid, n / 2, 0, n / 2); });
+        thread t4([&]() { rec(root->bottomRight = new Node(), grid, n / 2, n / 2, n / 2); });
+
+        // Wait for all threads to finish
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
+
+        auto end_time = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
+
+        cout << "Time taken for a parallel construction: " << duration.count() << " microseconds" << endl;
 
         return root;
     }
@@ -67,27 +95,33 @@ private:
     }
 };
 
-void runSequentialConstruction(vector<vector<int>>& grid, QuadTree quadTree) {
+void runParallelConstruction(vector<vector<int>>& grid) {
+    QuadTree quadTree;
+    quadTree.constructParallel(grid);
+}
+
+void runSequentialConstruction(vector<vector<int>>& grid) {
+    QuadTree quadTree;
     quadTree.constructSequential(grid);
 }
 
 int main() {
-
-    cout << "4 Threads Executing Construction in Parallel" << endl;
+    
     vector<vector<int>> grid1(50 * 1024, vector<int>(50 * 1024, 1));
-    vector<vector<int>> grid2(50 * 1024, vector<int>(50 * 1024, 1));
-    vector<vector<int>> grid3(50 * 1024, vector<int>(50 * 1024, 1));
-    vector<vector<int>> grid4(50 * 1024, vector<int>(50 * 1024, 1));
+    vector<vector<int>> grid2(50 * 1024, vector<int>(50 * 1024, 2));
+    vector<vector<int>> grid3(50 * 1024, vector<int>(50 * 1024, 3));
+    vector<vector<int>> grid4(50 * 1024, vector<int>(50 * 1024, 4));
 
-    QuadTree quadTree;
 
-    thread t1(runSequentialConstruction, ref(grid1), ref(quadTree));
-    thread t2(runSequentialConstruction, ref(grid2), ref(quadTree));
-    thread t3(runSequentialConstruction, ref(grid3), ref(quadTree));
-    thread t4(runSequentialConstruction, ref(grid4), ref(quadTree));
+    thread t1(runSequentialConstruction, ref(grid1));
+    thread t2(runSequentialConstruction, ref(grid2));
+    thread t3(runSequentialConstruction, ref(grid3));
+    thread t4(runSequentialConstruction, ref(grid4));
 
     auto start_time = chrono::high_resolution_clock::now();
 
+
+    // Wait for all threads to complete
     t1.join();
     t2.join();
     t3.join();
@@ -97,6 +131,19 @@ int main() {
     auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
 
     cout << "Time taken for parallel construction of 4 trees: " << duration.count() << " microseconds" << endl;
+
+
+    auto start_time2 = chrono::high_resolution_clock::now();
+
+    runSequentialConstruction(grid1);
+    runSequentialConstruction(grid2);
+    runSequentialConstruction(grid3);
+    runSequentialConstruction(grid4);
+
+    auto end_time2 = chrono::high_resolution_clock::now();
+    auto duration2 = chrono::duration_cast<chrono::microseconds>(end_time2 - start_time2);
+
+    cout << "Time taken for sequential construction of 4 trees: " << duration2.count() << " microseconds" << endl;
 
     return 0;
 }
